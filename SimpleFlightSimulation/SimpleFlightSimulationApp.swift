5ec9345ea5d3b0bdafaa2a7f18efe171dadcf45d
Steven Wijaya <steven.wijaya2001@gmail.com>
2023-02-16 10:56:49 +0700

    Initial Commit

---
+++ SimpleFlightSimulation/SimpleFlightSimulationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleFlightSimulationApp.swift
+//  SimpleFlightSimulation
+//
+//  Created by Steven Wijaya on 16.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct SimpleFlightSimulationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
